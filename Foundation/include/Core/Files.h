1a5c06b66442c98e2a51f7a4d8e6732d1c532696
Athang Gupte <xdevapps0@gmail.com>
2023-09-08 09:57:27 -0700

    Added read file function

--- Foundation/include/Core/Files.h
+++ Foundation/include/Core/Files.h
@@ -3,6 +3,29 @@
 
 namespace apex {
 
-	AxArray<char> readFile(char const* filename);
+	struct FileModeFlags
+	{
+		enum FileAccessMode : uint32
+		{
+			eRead  = 0x00000001,
+			eWrite = 0x00000002,
+		};
+
+		enum FileMode : uint32
+		{
+			eBinary   = 0x00000010,
+			eChar     = 0x00000020,
+			eWideChar = 0x00000040
+		};
+
+		enum FileCreateMode : uint32
+		{
+			eCreateNew    = 0x00000100,
+			eOpenExisting = 0x00000200
+		};
+	};
+	using FileMode = uint32;
+
+	AxArray<char> readFile(char const* filename, FileMode mode = FileModeFlags::eRead | FileModeFlags::eChar);
 
 }
--- Foundation/src/Files.cpp
+++ Foundation/src/Files.cpp
@@ -7,48 +7,52 @@
 
 namespace apex {
 
-	AxArray<char> readFile(char const* filename)
+	AxArray<char> readFile(char const* filename, FileMode mode)
 	{
 		AxArray<char> fileBuf;
 
 	#ifdef APEX_PLATFORM_WIN32
 		HANDLE hFile;
 
+		DWORD desiredAccess = (mode & FileModeFlags::eRead ? GENERIC_READ : 0) | (mode & FileModeFlags::eWrite ? GENERIC_WRITE : 0);
+		DWORD shareMode = (mode & FileModeFlags::eRead ? FILE_SHARE_READ : 0) | (mode & FileModeFlags::eWrite ? FILE_SHARE_WRITE : 0);
+		DWORD creationDisposition = mode & FileModeFlags::eOpenExisting ? OPEN_EXISTING : mode & FileModeFlags::eCreateNew ? CREATE_NEW : OPEN_ALWAYS;
+
 		hFile = CreateFile(
 			TEXT(filename),
-			GENERIC_READ,
-			FILE_SHARE_READ,
+			desiredAccess,
+			shareMode,
 			NULL,
-			OPEN_EXISTING,
+			creationDisposition,
 			FILE_ATTRIBUTE_NORMAL,
 			NULL);
 
 		if (INVALID_HANDLE_VALUE == hFile)
 		{
-			axAssertMsg(INVALID_HANDLE_VALUE == hFile, "Could not open file!");
+			axAssertMsg(false, "Could not open file!");
 			return fileBuf;
 		}
 
 		LARGE_INTEGER liFileSize;
 		if (FALSE == GetFileSizeEx(hFile, &liFileSize))
 		{
-			axAssertMsg(FALSE, "Could not retrieve file size!");
+			axAssertMsg(false, "Could not retrieve file size!");
 			CloseHandle(hFile);
 			return fileBuf;
 		}
 
-		int64 fileSize = liFileSize.QuadPart + 1;
+		int64 fileSize = liFileSize.QuadPart + (mode & FileModeFlags::eBinary ? 0 : 1);
 		fileBuf.resize(fileSize);
 		OVERLAPPED ol {};
 		DWORD dwBytesRead;
 		if (FALSE == ReadFile(hFile, fileBuf.data(), liFileSize.LowPart, &dwBytesRead, &ol))
 		{
-			axAssertMsg(FALSE, "Could not retrieve file size!");
+			axAssertMsg(false, "Could not retrieve file size!");
 			CloseHandle(hFile);
 			return fileBuf;
 		}
 
-		if (dwBytesRead > 0 && dwBytesRead <= fileSize - 1)
+		if ((mode & FileModeFlags::eBinary) == 0 && dwBytesRead > 0 && dwBytesRead <= fileSize - 1)
 		{
 			fileBuf[dwBytesRead] = '\0';
 		}
@@ -58,6 +62,7 @@ namespace apex {
 		return fileBuf;
 
 	#else
+		#error Not yet implemented correctly!
 		FILE* file;
 		(void)fopen_s(&file, filename, "r");
 
