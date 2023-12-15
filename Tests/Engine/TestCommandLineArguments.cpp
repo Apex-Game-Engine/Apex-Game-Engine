c544a97c5c9c7425ab258529ff434a4f70219e57
Athang Gupte <xdevapps0@gmail.com>
2023-12-15 05:04:32 -0800

    Added Game impl in Startup test. Added tests for cmdline args.

---
+++ Tests/Engine/TestCommandLineArguments.cpp
@@ -0,0 +1,65 @@
+﻿#include <gtest/gtest.h>
+#include "Api.h"
+#include "CommandLineArguments.h"
+
+TEST(TestCommandLineArguments, TestParser)
+{
+	apex::CommandLineArguments cmdLine;
+	cmdLine.defineOptions({
+		{ .shortName = "h", .longName = "headless" },
+		{ .shortName = "l", .longName = "load", .hasValue = true }
+	});
+
+	const char* argv[] = {
+		"-h",
+		"--load",
+		"Assets\\Levels\\mainLevel.apx"
+	};
+
+	cmdLine.parse(std::size(argv), argv);
+
+	int count = 0;
+	for (auto& opt : cmdLine.getSelectedOptions())
+	{
+		switch (count)
+		{
+		case 0:
+			EXPECT_STREQ(opt.longName, "headless");
+			break;
+		case 1:
+			EXPECT_STREQ(opt.longName, "load");
+			EXPECT_STREQ(opt.value, argv[2]);
+			break;
+		}
+		count++;
+	}
+}
+
+TEST(TestCommandLineArguments, TestCmdString)
+{
+	apex::CommandLineArguments cmdLine;
+	cmdLine.defineOptions({
+		{ .shortName = "h", .longName = "headless" },
+		{ .shortName = "l", .longName = "load", .hasValue = true }
+	});
+
+	const char* cmdline = "-h --load Assets\\Levels\\mainLevel.apx";
+
+	cmdLine.parse(cmdline);
+
+	int count = 0;
+	for (auto& opt : cmdLine.getSelectedOptions())
+	{
+		switch (count)
+		{
+		case 0:
+			EXPECT_STREQ(opt.longName, "headless");
+			break;
+		case 1:
+			EXPECT_STREQ(opt.longName, "load");
+			EXPECT_STREQ(opt.value, "Assets\\Levels\\mainLevel.apx");
+			break;
+		}
+		count++;
+	}
+}
--- Tests/Startup/TestWindow.cpp
+++ Tests/Startup/TestWindow.cpp
@@ -1,2 +1,16 @@
 #include <apex_pch.h>
+#include <Apex/Game.h>
+
+class MyGame : public apex::Game
+{
+public:
+	void run() override {}
+	void stop() override {}
+};
+
+apex::UniquePtr<apex::Game> apex::Game::Construct()
+{
+	return apex::static_unique_cast<Game>(apex::make_unique<MyGame>());
+}
+
 #include <Startup.h>
