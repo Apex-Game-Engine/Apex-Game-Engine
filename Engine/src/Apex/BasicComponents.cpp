a40e905a37d0876389a8f9ada62b5c628376ed9b
Athang Gupte <xdevapps0@gmail.com>
2024-05-03 18:00:48 -0700

    Fixed modified include

---
+++ Engine/src/Apex/BasicComponents.cpp
@@ -0,0 +1,10 @@
+﻿#include "apex_pch.h"
+#include "Apex/ECS/BasicComponents.h"
+
+#include "Apex/TypeInfo.h"
+
+namespace apex::ecs {
+
+	static const uint32 gTransformComponentIndex = core::TypeIndex<Transform>::value();
+	
+}
