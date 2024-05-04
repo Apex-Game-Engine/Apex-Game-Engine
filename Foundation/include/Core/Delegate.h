a470d036ff6ca4c8ef93bb66e45aff027e30c21f
Athang Gupte <xdevapps0@gmail.com>
2024-05-03 17:58:43 -0700

    Added Delegate type for function delegates

---
+++ Foundation/include/Core/Delegate.h
@@ -0,0 +1,101 @@
+﻿#pragma once
+#include <utility>
+
+#include "TypeTraits.h"
+
+
+namespace apex {
+
+	template <auto>
+	struct delegate_arg_t
+	{
+	    explicit delegate_arg_t() = default;
+	};
+
+	template <auto Candidate>
+	inline constexpr delegate_arg_t<Candidate> delegate_arg{};
+
+	template <typename>
+	struct Delegate;
+
+	template <typename Ret, typename... Args>
+	struct Delegate<Ret(Args...)>
+	{
+	    using function_type = Ret(const void*, Args...);
+	    using type = Ret(Args...);
+	    using result_type = Ret;
+	    using args_type_list = type_list<Args...>;
+
+	    constexpr Delegate() = default;
+
+	    template <auto Candidate, typename... Type>
+	    Delegate(delegate_arg_t<Candidate>, Type&&... value_or_instance)
+	    {
+	        set<Candidate>(std::forward<Type>(value_or_instance)...);
+	    }
+
+	    Delegate(function_type* function, const void* payload = nullptr)
+	    {
+	        set(function, payload);
+	    }
+
+	    template <auto Candidate>
+	    void set()
+	    {
+	        instance = nullptr;
+
+	        if constexpr(std::is_invocable_r_v<Ret, decltype(Candidate), Args...>)
+	        {
+	            fn = +[](const void*, Args... args) -> Ret
+	            {
+	                return Ret(std::invoke(Candidate, std::forward<Args>(args)...));
+	            };
+	        }
+	        else
+	        {
+	            [flag = false]() { static_assert(flag, "Delegate candidate is not invocable!"); }();
+	        }
+	    }
+
+	    template <auto Candidate, typename Type>
+	    void set(Type& value_or_instance)
+	    {
+	        instance = &value_or_instance;
+
+	        if constexpr(std::is_invocable_r_v<Ret, decltype(Candidate), Type&, Args...>)
+	        {
+	            fn = +[](const void* payload, Args... args) -> Ret
+	            {
+	                Type* value = static_cast<Type*>(const_cast<std::conditional_t<std::is_const_v<Type>, const void, void>*>(payload));
+	                return Ret(std::invoke(Candidate, *value, std::forward<Args>(args)...));
+	            };
+	        }
+	        else
+	        {
+	            [flag = false]() { static_assert(flag, "Delegate candidate is not invocable!"); }();
+	        }
+	    }
+
+	    void set(function_type* function, const void* payload = nullptr)
+	    {
+	        instance = payload;
+	        fn = function;
+	    }
+
+	    auto operator()(Args&&... args) const -> Ret
+	    {
+	        return fn(instance, std::forward<Args>(args)...);
+	    }
+
+	    explicit operator bool() const
+	    {
+	        return fn != nullptr;
+	    }
+
+	    const void *instance {nullptr};
+	    function_type *fn {nullptr};
+	};
+
+
+
+}
---
+++ Tests/Foundation/TestDelegate.cpp
@@ -0,0 +1,263 @@
+﻿#include <gtest/gtest.h>
+
+#include "Core/Delegate.h"
+
+namespace detail
+{
+	static int counter = 0;
+}
+
+void counter_inc()
+{
+	++detail::counter;
+}
+
+void counter_dec()
+{
+	--detail::counter;
+}
+
+void counter_set(int value)
+{
+	detail::counter = value;
+}
+
+TEST(TestDelegate, TestVoidReturnFreeFunction)
+{
+	EXPECT_EQ(detail::counter, 0);
+	{
+		apex::Delegate<void()> delegate;
+		
+		delegate.set<counter_inc>();
+		delegate();
+		EXPECT_EQ(detail::counter, 1);
+
+		delegate.set<counter_dec>();
+		delegate();
+		EXPECT_EQ(detail::counter, 0);
+
+	}
+
+	EXPECT_EQ(detail::counter, 0);
+	{
+		apex::Delegate<void(int)> delegate;	
+		delegate.set<counter_set>();
+		delegate(42);
+		EXPECT_EQ(detail::counter, 42);
+	}
+
+	counter_set(0);
+}
+
+TEST(TestDelegate, TestVoidReturnNonCapturingLambda)
+{
+	EXPECT_EQ(detail::counter, 0);
+	{
+		apex::Delegate<void()> delegate;
+		delegate.set<[]() { ++detail::counter; }>();
+		delegate();
+		EXPECT_EQ(detail::counter, 1);
+
+		delegate.set<[]() { --detail::counter; }>();
+		delegate();
+		EXPECT_EQ(detail::counter, 0);
+	}
+
+	EXPECT_EQ(detail::counter, 0);
+	{
+		apex::Delegate<void(int)> delegate;
+		delegate.set<[](int value) { detail::counter = value; }>();
+		delegate(42);
+		EXPECT_EQ(detail::counter, 42);
+	}
+}
+
+
+TEST(TestDelegate, TestVoidReturnCapturingLambda)
+{
+	EXPECT_TRUE(false); // This test fails, because the lambda is not copied into the delegate
+	//EXPECT_EQ(detail::counter, 0);
+	//{
+	//	int value = 42;
+	//	apex::Delegate<void()> delegate;
+	//	delegate.set<[&value]() { detail::counter = value; }>();
+	//	delegate();
+	//	EXPECT_EQ(detail::counter, 42);
+
+	//	value = 0;
+	//	delegate();
+	//	EXPECT_EQ(detail::counter, 0);
+	//}
+}
+
+TEST(TestDelegate, TestVoidReturnMemberFunction)
+{
+	struct Counter
+	{
+		int value = 0;
+		void inc() { ++value; }
+		void dec() { --value; }
+		void set(int val) { value = val; }
+	};
+
+	{
+		Counter counter;
+		apex::Delegate<void()> delegate;
+		delegate.set<&Counter::inc>(counter);
+		delegate();
+		EXPECT_EQ(counter.value, 1);
+
+		delegate.set<&Counter::dec>(counter);
+		delegate();
+		EXPECT_EQ(counter.value, 0);
+	}
+
+	EXPECT_EQ(detail::counter, 0);
+	{
+		Counter counter;
+		apex::Delegate<void(int)> delegate;
+		delegate.set<&Counter::set>(counter);
+		delegate(42);
+		EXPECT_EQ(counter.value, 42);
+	}
+}
+
+
+TEST(TestDelegate, TestMemberFunctionFromBaseClass)
+{
+	struct Base
+	{
+		int value = 0;
+		void inc() { ++value; }
+	};
+
+	struct Derived : Base
+	{
+		void dec() { --value; }
+	};
+
+	{
+		Derived derived;
+		apex::Delegate<void()> delegate;
+		delegate.set<&Base::inc>(derived);
+		delegate();
+		EXPECT_EQ(derived.value, 1);
+
+		delegate.set<&Derived::dec>(derived);
+		delegate();
+		EXPECT_EQ(derived.value, 0);
+	}
+}
+
+template <typename T>
+void counter_inc_template(T& counter)
+{
+	++counter;
+}
+
+TEST(TestDelegate, TestFreeFunctionTemplate)
+{
+	int counter = 0;
+	{
+		apex::Delegate<void(int&)> delegate;
+		delegate.set<counter_inc_template<int>>();
+		delegate(counter);
+		EXPECT_EQ(counter, 1);
+	}	
+}
+
+struct CounterInvoker
+{
+	apex::Delegate<void(const int&)> delegate;
+};
+
+struct Counter
+{
+	int value = 0;
+
+	constexpr Counter() = default;
+	Counter(CounterInvoker& invoker);
+
+	template <typename T>
+	void set(T const& val) { value = val; }
+};
+
+Counter::Counter(CounterInvoker& invoker)
+{
+	invoker.delegate.set<&Counter::set<int>>(*this);
+}
+
+TEST(TestDelegate, TestMemberFunctionTemplate)
+{
+	{
+		Counter counter;
+		apex::Delegate<void(const int&)> delegate;
+		delegate.set<&Counter::set<int>>(counter);
+		delegate(42);
+		EXPECT_EQ(counter.value, 42);
+	}
+}
+
+TEST(TestDelegate, TestMemberFunctionTemplateWithDifferentType)
+{
+	{
+		Counter counter;
+		apex::Delegate<void(const float&)> delegate;
+		delegate.set<&Counter::set<float>>(counter);
+		delegate(42.0f);
+		EXPECT_EQ(counter.value, 42);
+	}
+}
+
+TEST(TestDelegate, TestMemberFunctionTemplateWithThisPointer)
+{
+	{
+		CounterInvoker invoker;
+		Counter counter(invoker);
+
+		invoker.delegate(42);
+		
+		EXPECT_EQ(counter.value, 42);
+	}
+}
+
+template <typename Derived>
+struct CounterTemplate
+{
+	int value = 0;
+
+	constexpr CounterTemplate() = default;
+	CounterTemplate(CounterInvoker& invoker);
+
+	template <typename U>
+	void set(U const& val) { value = val; }
+};
+
+template <typename Derived>
+CounterTemplate<Derived>::CounterTemplate(CounterInvoker& invoker)
+{
+	invoker.delegate.set<&Derived::template set<int>>(static_cast<Derived&>(*this));
+}
+
+struct DoubleCounter : public CounterTemplate<DoubleCounter>
+{
+	constexpr DoubleCounter() : CounterTemplate() {}
+	DoubleCounter(CounterInvoker& invoker) : CounterTemplate(invoker) {}
+
+	template <typename U>
+	void set(U const& val) { CounterTemplate::set(val); }
+
+	template <>
+	void set(int const& val) { value = val * 2; }
+};
+
+TEST(TestDelegate, TestMemberFunctionTemplateFromBaseClass)
+{
+	{
+		CounterInvoker invoker;
+		DoubleCounter counter(invoker);
+
+		counter.set(42);
+		EXPECT_EQ(counter.value, 84);
+	}
+}
