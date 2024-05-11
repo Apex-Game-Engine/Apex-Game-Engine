a02b8c1f2614bef0b0ee66e796bb98578bb3dc85
Athang Gupte <xdevapps0@gmail.com>
2024-05-11 13:45:50 -0700

    Added integer types to vertex data

--- Graphics/include/Graphics/Geometry/Vertex.h
+++ Graphics/include/Graphics/Geometry/Vertex.h
@@ -17,6 +17,10 @@ namespace gfx {
 		eVec2,
 		eVec3,
 		eVec4,
+		eInt,
+		eIntVec2,
+		eIntVec3,
+		eIntVec4,
 
 		eInvalid,
 		eCOUNT = eInvalid,
@@ -30,6 +34,7 @@ namespace gfx {
 		eTangent,
 		eJointWeight,
 		eJointIndex,
+		eMaterialIndex,
 		eColor,
 
 		eCOUNT
@@ -39,13 +44,14 @@ namespace gfx {
 	{
 		switch (attr)
 		{
-		case VertexAttribute::ePosition:    return AttributeType::eVec3;
-		case VertexAttribute::eTexCoord:    return AttributeType::eVec2;
-		case VertexAttribute::eNormal:      return AttributeType::eVec3;
-		case VertexAttribute::eTangent:     return AttributeType::eVec3;
-		case VertexAttribute::eJointWeight: return AttributeType::eVec4;
-		case VertexAttribute::eJointIndex:  return AttributeType::eFloat;
-		case VertexAttribute::eColor:       return AttributeType::eVec4;
+		case VertexAttribute::ePosition:      return AttributeType::eVec3;
+		case VertexAttribute::eTexCoord:      return AttributeType::eVec2;
+		case VertexAttribute::eNormal:        return AttributeType::eVec3;
+		case VertexAttribute::eTangent:       return AttributeType::eVec3;
+		case VertexAttribute::eJointWeight:   return AttributeType::eVec4;
+		case VertexAttribute::eJointIndex:    return AttributeType::eIntVec4;
+		case VertexAttribute::eMaterialIndex: return AttributeType::eInt;
+		case VertexAttribute::eColor:         return AttributeType::eVec4;
 		}
 		axAssertMsg(false, "Invalid vertex attribute type!");
 	}
@@ -54,10 +60,14 @@ namespace gfx {
 	{
 		switch (type)
 		{
+		case AttributeType::eInt:
 		case AttributeType::eFloat: return sizeof(float);
-		case AttributeType::eVec2: return sizeof(math::Vector2);
-		case AttributeType::eVec3: return sizeof(math::Vector3);
-		case AttributeType::eVec4: return sizeof(math::Vector4);
+		case AttributeType::eIntVec2:
+		case AttributeType::eVec2:  return sizeof(math::Vector2);
+		case AttributeType::eIntVec3:
+		case AttributeType::eVec3:  return sizeof(math::Vector3);
+		case AttributeType::eIntVec4:
+		case AttributeType::eVec4:  return sizeof(math::Vector4);
 		}
 	}
 
@@ -69,6 +79,10 @@ namespace gfx {
 		case AttributeType::eVec2:  return VK_FORMAT_R32G32_SFLOAT;
 		case AttributeType::eVec3:  return VK_FORMAT_R32G32B32_SFLOAT;
 		case AttributeType::eVec4:  return VK_FORMAT_R32G32B32A32_SFLOAT;
+		case AttributeType::eInt:   return VK_FORMAT_R32_SINT;
+		case AttributeType::eIntVec2: return VK_FORMAT_R32G32_SINT;
+		case AttributeType::eIntVec3: return VK_FORMAT_R32G32B32_SINT;
+		case AttributeType::eIntVec4: return VK_FORMAT_R32G32B32A32_SINT;
 		}
 	}
 
@@ -175,19 +189,31 @@ namespace gfx {
 	static_assert(Vertex_P0_TC0::attrs[1] == VertexAttribute::eTexCoord);
 	static_assert(Vertex_P0_TC0::size() == sizeof(Vertex_P0_TC0));
 
-	struct Vertex_P0_JI0_C0 : Vertex<VertexAttribute::ePosition, 
-									 VertexAttribute::eJointIndex, 
+	struct Vertex_P0_M0_C0 : Vertex<VertexAttribute::ePosition, 
+									 VertexAttribute::eMaterialIndex, 
 									 VertexAttribute::eColor>
 	{
 		math::Vector3 position;
-		float32 jointIndex{};
+		float32 materialIndex{};
 		math::Vector4 color;
+		//int jointIndex[4];
 	};
 
-	static_assert(Vertex_P0_JI0_C0::attrs[0] == VertexAttribute::ePosition);
-	static_assert(Vertex_P0_JI0_C0::attrs[1] == VertexAttribute::eJointIndex);
-	static_assert(Vertex_P0_JI0_C0::attrs[2] == VertexAttribute::eColor);
-	static_assert(Vertex_P0_JI0_C0::size() == sizeof(Vertex_P0_JI0_C0));
+	static_assert(Vertex_P0_M0_C0::attrs[0] == VertexAttribute::ePosition);
+	static_assert(Vertex_P0_M0_C0::attrs[1] == VertexAttribute::eMaterialIndex);
+	static_assert(Vertex_P0_M0_C0::attrs[2] == VertexAttribute::eColor);
+	static_assert(Vertex_P0_M0_C0::size() == sizeof(Vertex_P0_M0_C0));
+
+	// packed Vertex P,TC,N,T,JI,JW,M
+	// P: position
+	// TC: texcoord
+	// N: normal
+	// T: tangent
+	// JI: joint index
+	// JW: joint weight
+	// M: material index
+	// | P0 P1 P2 TC0 | N0 N1 N2 TC1 | T0 T1 T2 M | JI0 JI1 JI2 JI3 | JW0 JW1 JW2 JW3 |
+	// Can be used with SSBO
 
 }
 }
