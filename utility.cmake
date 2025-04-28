4800048a31b411ce8c836dd3dce725a871e4340c
Athang Gupte <xdevapps0@gmail.com>
2025-04-28 03:14:46 -0700

    CMake utility - `add_shader_library`

--- utility.cmake
+++ utility.cmake
@@ -45,30 +45,39 @@ endfunction(ignore_precompiled_header)
 
 
 # shader compiler taken from https://stackoverflow.com/a/60472877
-function(target_vulkan_shaders Target)
+function(add_shader_library Target)
     set(multiValueArgs SOURCES INCLUDES)
     cmake_parse_arguments(PARSE_ARGV 1 arg "" "" "${multiValueArgs}")
+	set(SHADER_SOURCES)
     set(SPIRV_OUTPUTS)
     foreach(source ${arg_SOURCES})
+		cmake_path(ABSOLUTE_PATH source NORMALIZE)
         cmake_path(GET source FILENAME source_filename)
         set(spirv_output "${CMAKE_CURRENT_BINARY_DIR}/spv/${source_filename}.spv")
         message(STATUS "Adding ${source_filename} as shader source")
-        add_custom_command(
-            OUTPUT ${spirv_output}
-            DEPENDS ${source}
-            COMMAND
-                ${GLSLC}
-                --target-env=vulkan1.3
-                -o ${spirv_output}
-                ${source}
-            COMMENT "Compiling shader file : ${source}"
-            VERBATIM
-        )
-        list(APPEND SPIRV_OUTPUTS ${spirv_output})
+        list(APPEND SHADER_SOURCES ${source})
+		list(APPEND SPIRV_OUTPUTS ${spirv_output})
     endforeach()
-    add_custom_target(${Target} ALL DEPENDS ${SPIRV_OUTPUTS} ${arg_SOURCES} ${arg_INCLUDES})
+	set(TargetSpv "${Target}_SPV")
+    add_custom_target(${TargetSpv} ALL DEPENDS ${SPIRV_OUTPUTS} ${arg_SOURCES} ${arg_INCLUDES})
+	add_library(${Target} INTERFACE)
+	foreach(source spirv_output IN ZIP_LISTS SHADER_SOURCES SPIRV_OUTPUTS)
+		add_custom_command(
+			OUTPUT ${spirv_output}
+			DEPENDS ${source}
+			COMMAND
+				${GLSLC}
+				--target-env=vulkan1.3
+				-I "$<TARGET_PROPERTY:${Target},INTERFACE_INCLUDE_DIRECTORIES>"
+				-o ${spirv_output}
+				${source}
+			COMMENT "Compiling shader file : ${source}"
+			VERBATIM
+		)
+	endforeach()
+	target_sources(${Target} PRIVATE ${arg_SOURCES} ${arg_INCLUDES})
+	add_dependencies(${Target} ${TargetSpv})
 endfunction()
-
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL Clang AND "x${CMAKE_CXX_SIMULATE_ID}" STREQUAL "xMSVC")
     set(APEX_COMPILER_TOOLSET_CLANG_CL TRUE)
